55c465774f84179c11f2a9aa53407c8f9aa1eae7
bradyDarren <dcgbfinancial@gmail.com>
2025-03-17 01:40:07 -0500

    additional practice

--- practice.py
+++ practice.py
@@ -1,13 +1,37 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
+MY_LAT = 29.785786
+MY_LONG = -95.824394
+
+# response = requests.get(url='http://api.open-notify.org/iss-now.json')
+# response.raise_for_status()
+
+# data = response.json()
+# print(data)
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]['latitude']
+
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+parameters = {
+    'lat':MY_LAT,
+    'lng':MY_LONG,
+    'formatted':0,
+    'tzid':'CST'
+}
+
+response = requests.get(url='https://api.sunrise-sunset.org/json', params=parameters)
 response.raise_for_status()
 
 data = response.json()
 print(data)
+sunrise = data['results']['sunrise']
+sunset = data['results']['sunset']
+print(sunrise)
+print(sunset)
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]['latitude']
-
-iss_position = (longitude, latitude)
-print(iss_position)
+time_now = datetime.now()
+print(time_now)
