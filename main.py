41e4cd69f742249887fa9fb86eb829d2fd6495ca
bradyDarren <dcgbfinancial@gmail.com>
2025-03-17 02:35:26 -0500

    completion of fetch_data function, began work on time_conv function

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+import smtplib
+
+# setting coordinates
+MY_LAT = 38.545502
+MY_LONG = -106.926903
+MY_EMAIL = 'bradydarren056@gmail.com'
+
+# 1. Fetch data from API's
+
+def fetch_data():
+
+    data_obj = []
+
+    parameters = {
+        'lat':MY_LAT,
+        'lng':MY_LONG,
+        'formatted':1
+    }
+
+    iss_response = requests.get(url = 'http://api.open-notify.org/iss-now.json')
+    iss_response.raise_for_status()
+
+    ss_response = requests.get(url = 'https://api.sunrise-sunset.org/json',params=parameters)
+    ss_response.raise_for_status()
+
+    iss_data = iss_response.json()
+    ss_data = ss_response.json()
+    
+    data_obj.append(iss_data)
+    data_obj.append(ss_data)
+
+    return data_obj
+
+# 2. Convert time from UTC to CST
+
+def time_conv(utc_time = None):
+    current_dt = datetime.now()
+    sunrise = fetch_data()[1]['results']['sunrise']
+    print(sunrise)
+time_conv()
+    
+
+# 2. Check and see if it is currently dark.
+
+# 3. If dark send email to email address telling notifiying us to look up. 
