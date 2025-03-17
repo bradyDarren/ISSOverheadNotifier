b124a729bda630ea6d500f703ad594ff742f0c74
bradyDarren <dcgbfinancial@gmail.com>
2025-03-16 23:18:07 -0500

    creation of the get_quote method

--- kanye.py
+++ kanye.py
@@ -1,9 +1,14 @@
 from tkinter import * 
-
+import requests
 
 
 def get_quote():
-    pass
+    response = requests.get(url='https://api.kanye.rest')
+    response.raise_for_status()
+
+    quote = response.json()['quote']
+
+    canvas.itemconfig(quote_text, text = quote)
 
 window = Tk()
 window.title("Kaye Says ...")
